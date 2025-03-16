0d68862f15ba67870ac9048b4f9a76d399b162af
ysoktsk <hnkc.ysoktsk@gmail.com>
2025-03-16 23:44:26 +0900

    chore: setup

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginTS from '@typescript-eslint/eslint-plugin'
+import eslintParserTS from '@typescript-eslint/parser'
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: eslintParserTS,
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTS,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': 'error',
+      ...eslintConfigPrettier.rules,
+    },
+  },
+]
